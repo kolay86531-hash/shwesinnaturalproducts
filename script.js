66b63c1abd4c6f0b54d53ccdc6ea870e39223f3e
kolay86531-hash <kolay86531@gmail.com>
2026-07-25 17:52:59 +0630

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,50 @@ document.body.classList.toggle("dark-mode");
 };
 
 }
+//==========================
+// LIGHTBOX
+//==========================
+
+const galleryImages=document.querySelectorAll(".gallery-grid img");
+
+const lightbox=document.getElementById("lightbox");
+
+const lightboxImg=document.getElementById("lightbox-img");
+
+const closeBtn=document.querySelector(".close-lightbox");
+
+galleryImages.forEach(img=>{
+
+img.addEventListener("click",()=>{
+
+lightbox.style.display="flex";
+
+lightboxImg.src=img.src;
+
+});
+
+});
+
+if(closeBtn){
+
+closeBtn.addEventListener("click",()=>{
+
+lightbox.style.display="none";
+
+});
+
+}
+
+if(lightbox){
+
+lightbox.addEventListener("click",(e)=>{
+
+if(e.target===lightbox){
+
+lightbox.style.display="none";
+
+}
+
+});
+
+}
